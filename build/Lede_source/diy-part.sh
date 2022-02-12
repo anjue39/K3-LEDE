94c6ea62d3010eb1ae413e9d38495065eb703077
holoto <laibin45@gmail.com>
2022-02-12 12:35:27 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -9,10 +9,14 @@
 
 #  src-git small8 https://github.com/kenzok8/small-package
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small-package https://github.com/kenzok8/small-package' feeds.conf.default
 
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
 
 echo '添加lwz322的K3屏幕插件'
